d313cccf090c8c395a231d526f4c47f60685c72e
Coltontvb <coltontvb@gmail.com>
2021-02-12 12:57:57 -0600

    Add integration for stripe

---
+++ public/js/front-checkout.js
@@ -0,0 +1,26 @@
+// Create an instance of the Stripe object with your publishable API key
+const stripe = Stripe("pk_test_51IJCF4EBTyBaxuv5QgnfjNhWTruxs2xQMaqXgDFYDlTZpPoSjJJub0IjkRcM3UaZsH1J6vtTHTyYefSHrGQ37pWt00JmQREhNj");
+
+const checkoutBtn = document.getElementById("checkoutBtn");
+checkoutBtn.addEventListener("click", function () {
+  fetch("payment/create-checkout-session", {
+    method: "POST",
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (session) {
+      return stripe.redirectToCheckout({ sessionId: session.id });
+    })
+    .then(function (result) {
+      // If redirectToCheckout fails due to a browser or network
+      // error, you should display the localized error message to your
+      // customer using error.message.
+      if (result.error) {
+        alert(result.error.message);
+      }
+    })
+    .catch(function (error) {
+      console.error("Error:", error);
+    });
+});
--- routes/payment.js
+++ routes/payment.js
@@ -1,80 +1,34 @@
 const router = require(`express`).Router();
-const paypal = require(`paypal-rest-sdk`);
+const stripe = require(`stripe`)(process.env.STRIPE_SK);
 
-// configure paypal with the credentials you got when you created your paypal app
-paypal.configure({
-    'mode': `sandbox`, //sandbox or live 
-    'client_id': `${process.env.PP_CID}`, // please provide your client id here 
-    'client_secret': `${process.env.PP_SID}` // provide your client secret here 
+//Create a customer and store them based on their input
+router.post('/create-checkout-session', async (req, res) => {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      line_items: [
+        {
+          price_data: {
+            currency: 'cad',
+            product_data: {
+              name: 'IT Consulting',
+            },
+            unit_amount: 99.99 * 100,
+          },
+          quantity: 1,
+        },
+      ],
+      mode: 'payment',
+      success_url: `${process.env.YOUR_DOMAIN}/payment/payment-success`,
+      cancel_url: `${process.env.YOUR_DOMAIN}/payment/payment-success`,
+    });
+    res.json({ id: session.id });
   });
 
-router.post(`/purchase`, (req, res) => {
-        // create payment object 
-        var create_payment = {
-        "intent": "sale",
-        "payer": {
-            "payment_method": "paypal"
-        },
-        "redirect_urls": {
-            "return_url": `${process.env.PP_RURL}`,
-            "cancel_url": `${process.env.PP_CURL}`
-        },
-        "transactions": [{
-            "item_list": {
-                "items": [{
-                    "name": "IT Consulting",
-                    "sku": "pd_1_0_0",
-                    "price": "99.99",
-                    "currency": "USD",
-                    "quantity": 1
-                }]
-            },
-            "amount": {
-                "currency": "USD",
-                "total": "99.99"
-            },
-            "description": "This is the payment description."
-        }]
-    };
-        //Make the payment
-        paypal.payment.create(create_payment, (err, payment) => {
-            if (err){
-                console.log(err)
-            } else {
-                if(payment.payer.payment_method === `paypal`){
-                    req.paymentId = payment.id;
-                    var r_url;
-                    console.log(payment);
-                    for (var i=0; i<payment.links.length; i++){
-                        var link = payment.links[i];
-                        if (link.method === `REDIRECT`) {
-                            r_url = link.href;
-                        }          
-                    }
-                }
-                res.redirect(r_url);
-            }
-        });
-});
-
 router.get(`/payment-success`, (req, res) => {
-    var paymentId = req.query.paymentId;
-    var payerId = {'payer_id': req.query.PayerID};
-    paypal.payment.execute(paymentId, payerId, (err, payment) => {
-        if (err){
-            console.log(err);
-        } else {
-            if (payment.state == 'approved'){
-                res.render(`payment/payment-success`);
-            } else {
-                res.redirect(`payment/payment-error`);
-            }
-        }
-    })
     res.render(`payment/payment-success`);
 });
 
-router.get(`payment/payment-error`, (req, res) => {
+router.get(`/payment-error`, (req, res) => {
     res.render(`payment/payment-error`);
 });
 
